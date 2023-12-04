84f6d4e3f499df63122d84e027ea0c9cd63f4333
andrembpontes <andre.pontes@cypher-sys.com>
2023-12-04 15:15:08 +0000

    update

--- lua/zion/configs/neovide/style.lua
+++ lua/zion/configs/neovide/style.lua
@@ -4,6 +4,6 @@ if vim.fn.has("unix") == 1 then
 	if uname == "Darwin\n" then
 		vim.opt.guifont = "JetBrainsMono Nerd Font:h15"
 	else
-		vim.opt.guifont = "JetBrains Mono 8"
+		vim.opt.guifont = "JetBrainsMono Nerd Font:h9"
 	end
 end
