08c8ac1a041ca75abca94d6e93af0fc4b4a9dc4e
Andre Pontes <andrembpontes@gmail.com>
2024-07-23 23:56:53 +0100

    update

--- lua/zion/plugins/workspaces.lua
+++ lua/zion/plugins/workspaces.lua
@@ -1,29 +1,30 @@
 return {
-    {
-        "andrembpontes/project.nvim",
+	{
+		"andrembpontes/project.nvim",
+		branch = "add-workspace-folders-support",
 
-        -- TODO replace with original repo once PR gets merged
-        -- https://github.com/ahmedkhalf/project.nvim/pull/136
-        --"ahmedkhalf/project.nvim",
+		-- TODO replace with original repo once PR gets merged
+		-- https://github.com/ahmedkhalf/project.nvim/pull/136
+		--"ahmedkhalf/project.nvim",
 
-        dir = '~/development/project.nvim',
-        opts = {
-            detection_methods = { "lsp", "pattern" },
-            patterns = { ".git" },
-            -- Table of lsp clients to ignore by name
-            -- eg: { "efm", ... }
-            ignore_lsp = { "efm" },
+		-- dir = '~/development/project.nvim',
+		opts = {
+			detection_methods = { "lsp", "pattern" },
+			patterns = { ".git" },
+			-- Table of lsp clients to ignore by name
+			-- eg: { "efm", ... }
+			ignore_lsp = { "efm" },
 
-            datapath = vim.fn.stdpath("data"),
+			datapath = vim.fn.stdpath("data"),
 
-            -- What scope to change the directory, valid options are
-            -- * global (default)
-            -- * tab
-            -- * win
-            scope_chdir = 'global',
-        },
+			-- What scope to change the directory, valid options are
+			-- * global (default)
+			-- * tab
+			-- * win
+			scope_chdir = "global",
+		},
 
-        main = 'project_nvim',
-        config = true,
-    },
+		main = "project_nvim",
+		config = true,
+	},
 }
